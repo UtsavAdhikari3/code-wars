a7a142a290248e786459766c288db86fbcb4c63a
UtsavAdhikari3 <utsavadhikari99@gmail.com>
2024-04-24 19:51:43 +0545

    rev the words if length is greater than 40

---
+++ python/spin_words.py
@@ -0,0 +1,34 @@
+# Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters 
+# reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one
+# word is present.
+
+# Examples:
+
+# "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+# "This is a test        --> "This is a test" 
+# "This is another test" --> "This is rehtona test"
+
+
+def spin_words(sentence):
+    list1 = sentence.split()
+    list2 = []
+    count = 0
+    
+    for i in list1:
+        if len(i)>=5:
+            i = i[::-1]
+            list2 = list2 + [i]
+        else:
+            list2 = list2 + [i]
+            
+    
+    return ' '.join(list2)
+        
+      
+sentence = "Hey fellow warriors"
+
+print(spin_words(sentence))
+
+        
+        
+            
